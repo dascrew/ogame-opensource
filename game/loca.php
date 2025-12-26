e6314cf3f10685fdc1047dddc113172a3ebdf3fe
dascrew <ieuanapwilliams@gmail.com>
2025-12-26 22:25:09 +0000

    Refactor language handling in loca.php for improved readability and maintainability

--- game/loca.php
+++ game/loca.php
@@ -5,42 +5,42 @@
 // But actually you don't need it all at same time, only some of them (except very important ones)
 
 // Supported language list
-$Languages = array ( 
-#    'ae' => "اللغة العربية", 
-#    'ar' => "Español", 
-#    'ba' => "Босански", 
-#    'bg' => "Български", 
-#    'cn' => "中文", 
-#    'cz' => "Český", 
-    'de' => "Deutsch", 
-#    'dk' => "Dansk", 
-    'en' => "English", 
-    'es' => "Español", 
-#    'fi' => "Suomi", 
-    'fr' => "Français", 
-#    'gr' => "Ελληνικά", 
-#    'hr' => "Hrvatski", 
-#    'hu' => "Magyar", 
-    'it' => "Italiano", 
-    'jp' => "日本語", 
-#    'lt' => "Lietuvių", 
-#    'lv' => "Latviešu", 
-#    'nl' => "Nederlandse", 
-#    'no' => "Norsk", 
-#    'pl' => "Polski", 
-#    'pt' => "Português", 
-#    'ro' => "Română", 
-#    'rs' => "Српски", 
-    'ru' => "Русский", 
-#    'sk' => "Slovenčina", 
-#    'se' => "Svenska", 
-#    'tr' => "Türkçe", 
-#    'tw' => "臺灣話", 
+$Languages = [
+#    'ae' => "اللغة العربية",
+#    'ar' => "Español",
+#    'ba' => "Босански",
+#    'bg' => "Български",
+#    'cn' => "中文",
+#    'cz' => "Český",
+    'de' => 'Deutsch',
+#    'dk' => "Dansk",
+    'en' => 'English',
+    'es' => 'Español',
+#    'fi' => "Suomi",
+    'fr' => 'Français',
+#    'gr' => "Ελληνικά",
+#    'hr' => "Hrvatski",
+#    'hu' => "Magyar",
+    'it' => 'Italiano',
+    'jp' => '日本語',
+#    'lt' => "Lietuvių",
+#    'lv' => "Latviešu",
+#    'nl' => "Nederlandse",
+#    'no' => "Norsk",
+#    'pl' => "Polski",
+#    'pt' => "Português",
+#    'ro' => "Română",
+#    'rs' => "Српски",
+    'ru' => 'Русский',
+#    'sk' => "Slovenčina",
+#    'se' => "Svenska",
+#    'tr' => "Türkçe",
+#    'tw' => "臺灣話",
 #    'ua' => "Українська",
-);
+];
 
 // Default language if you have to make a choice. Since the project is already quite well internationalized - the default language is English.
-$DefaultLanguage = "en";
+$DefaultLanguage = 'en';
 
 //
 // The global language is set during the creation of a user session.
@@ -48,10 +48,10 @@ $DefaultLanguage = "en";
 
 $loca_lang = $DefaultLanguage;        // Language used. Can be changed at any time.
 
-$LOCA = array ();        // all the keys are in here.
+$LOCA = [];        // all the keys are in here.
 
 // This method can be used to bulk replace the original project name (OGame) with your project name (e.g. SpaceWars).
-function loca_subst_ogame ($text)
+function loca_subst_ogame($text)
 {
     //return str_replace ("OGame", "SpaceWars", $text);
     return $text;
@@ -59,35 +59,54 @@ function loca_subst_ogame ($text)
 
 // Return the value of the key. The latest version is returned.
 // If there is no connection to the LOCA or no such key exists, return the key name.
-function loca ($key)
+function loca($key)
 {
     global $LOCA, $loca_lang;
-    if ( !isset ( $LOCA[$loca_lang][$key] ) ) return $key;
-    else return loca_subst_ogame ($LOCA[$loca_lang][$key]);
+    if (!isset($LOCA[$loca_lang][$key])) {
+        return $key;
+    } else {
+        return loca_subst_ogame($LOCA[$loca_lang][$key]);
+    }
 }
 
 // Similar to regular loca(), but the language is selected from a method parameter rather than a global variable.
 // It is used when it is necessary to work simultaneously with several languages (e.g. battle reports for players with different languages).
-function loca_lang ($key, $lang)
+function loca_lang($key, $lang)
 {
     global $LOCA;
-    if ( !isset ( $LOCA[$lang][$key] ) ) return $key;
-    else return loca_subst_ogame ($LOCA[$lang][$key]);
+    if (!isset($LOCA[$lang][$key])) {
+        return $key;
+    } else {
+        return loca_subst_ogame($LOCA[$lang][$key]);
+    }
 }
 
 // Add a set of language keys.
-function loca_add ( $section, $lang='en' )
+function loca_add($section, $lang = 'en')
 {
-    global $LOCA, $Languages;
+    global $LOCA, $Languages, $DefaultLanguage;
 
     // Check if the language is on the list (to exclude injections)
     $found = false;
-    foreach ($Languages as $i=>$name ) {
-        if ( $i === $lang) { $found = true; break; }
+    foreach ($Languages as $i => $name) {
+        if ($i === $lang) {
+            $found = true;
+            break;
+        }
     }
-    if ( !$found ) return;
+    if (!$found) {
+        return;
+    }
 
-    include_once "loca/".$lang."_".$lang."/".$section.".php";
+    $path = 'loca/' . $lang . '_' . $lang . '/' . $section . '.php';
+    if (file_exists($path)) {
+        include_once $path;
+        return;
+    }
+
+    // Fallback to default language if requested locale file is missing
+    $fallback = 'loca/' . $DefaultLanguage . '_' . $DefaultLanguage . '/' . $section . '.php';
+    if (file_exists($fallback)) {
+        include_once $fallback;
+    }
 }
-
-?>
